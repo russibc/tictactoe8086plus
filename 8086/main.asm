104154f5c4c18552f047e2b1065096d0e45d3a73
heybruna <25191139+heybruna@users.noreply.github.com>
2017-08-19 17:25:05 -0300

    Update main.asm

--- 8086/main.asm
+++ 8086/main.asm
@@ -1,3 +1,45 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;										  ;;
+;;			     ___   ___   ___    __                                ;;
+;;			    / _ \ / _ \ / _ \  / /                                ;;
+;;			   | (_) | | | | (_) |/ /_                                ;;
+;;			    > _ <| | | |> _ <| '_ \                               ;;
+;;			   | (_) | |_| | (_) | (_) |                              ;;
+;;			    \___/ \___/ \___/ \___/                               ;;
+;;										  ;;
+;;	  _______ _____ _____ _______       _____ _______ ____  ______            ;;
+;;	 |__   __|_   _/ ____|__   __|/\   / ____|__   __/ __ \|  ____|           ;;
+;;	    | |    | || |       | |  /  \ | |       | | | |  | | |__              ;;
+;;	    | |    | || |       | | / /\ \| |       | | | |  | |  __|             ;;
+;;	    | |   _| || |____   | |/ ____ \ |____   | | | |__| | |____            ;;
+;;	    |_|  |_____\_____|  |_/_/    \_\_____|  |_|  \____/|______|           ;;
+;;										  ;;
+;;			  _____  _     _    _  _____ 				  ;;
+;;			 |  __ \| |   | |  | |/ ____|				  ;;
+;;			 | |__) | |   | |  | | (___  				  ;;
+;;			 |  ___/| |   | |  | |\___ \ 				  ;;
+;;			 | |    | |___| |__| |____) |				  ;;
+;;			 |_|    |______\____/|_____/ 				  ;;
+;;                                                   				  ;;
+;;                                                  				  ;;
+;;										  ;;								      										              ;;
+;;	 Copyright (C) 2017 Bruna Caroline Russi				  ;;
+;;                                                                                ;;
+;;       This program is free software: you can redistribute it and/or modify     ;;
+;;       it under the terms of the GNU General Public License as published by     ;;
+;;	 the Free Software Foundation, either version 3 of the License, or        ;;
+;;	 (at your option) any later version.                                      ;;
+;;                                                                                ;;
+;;	 This program is distributed in the hope that it will be useful,          ;;
+;;	 but WITHOUT ANY WARRANTY; without even the implied warranty of           ;;
+;;	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ;;
+;;	 GNU General Public License for more details.                             ;;
+;;                                                                                ;;
+;;	 You should have received a copy of the GNU General Public License        ;;
+;;	 along with this program.  If not, see <http://www.gnu.org/licenses/>.    ;;
+;;                                                                                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 .MODEL	SMALL
 
 ;8253
